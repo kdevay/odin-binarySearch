5b33424d000c5df5c7fde99b2b2bc404175e3c5f
Katherine de Vay <katdevay2@gmail.com>
2022-12-20 11:36:32 -0600

    Build tree and node classes and begin testing

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-// Node class:
-// - value
-// - left child
-// - right child
+// Node class
+class Node {
+    constructor(int) {
+        this.value = int;
+        this.left = null;
+        this.right = null;
+    }
+}
 
 // Tree class
 // - accepts an array when initialized
 // - root attribute (assigned by buildTree)
+class Tree {
+    constructor(){
+        this.root = null;
+    }
+    buildTree(array, startIndex, endIndex) {
+        // Base case
+        if (startIndex > endIndex){return NULL}
+        // Find root index
+        let i =  Math.round((startIndex + endIndex ) / 2);
+        this.root = new Node(array[i]); // Make root node
+        // Build branches
+        this.root.left = buildTree(array, startIndex, i - 1);
+        this.root.right = buildTree(array, i + 1, endIndex);
+        return this.root;
+    }
+}
 
-// BuildTree Function 
-// - converts an array of numbers into a balanced binary tree of Node objects 
-// - The buildTree function should return the root node.
+let arr = [10, 19, 50, 100, 200, 400, 1000,];
+
+let tree = new Tree();
+tree = tree.buildTree(arr, 0, arr.length - 1);
+console.log(tree);
