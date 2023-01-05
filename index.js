b0261671f7eaaeaa03e7859da116e4023a465d85
Katherine de Vay <katdevay2@gmail.com>
2023-01-05 11:16:38 -0600

    Debug Height, add depth function

--- index.js
+++ index.js
@@ -26,7 +26,30 @@ function getChildValues(node, deleteMe, arr){ // Recursively gets values from a 
         return 0;
     });
 }
-
+function helpOrder(type, node, array, func) {
+    if (type === 'in'){ // InOrder
+        if (node == null) { return array}
+        helpOrder('in', node.left, array, func); // Left side recursion
+        if (func !== undefined) { func(node) } // Apply function to node
+        array.push(node); // Add node to array
+        helpOrder('in', node.right, array, func); // Right side recursion
+        return array;
+    } else if (type === 'pre'){ //PreOrder
+        if (node == null) {return array}
+        if (func !== undefined) {func(node)} // Apply function to node
+        array.push(node); // Add node to array
+        helpOrder('pre', node.left, array, func); // Left side recursion
+        helpOrder('pre', node.right, array, func); // Right side recursion
+        return array;
+    } else { // PostOrder
+        if (node == null) {return array}
+        helpOrder('post', node.left, array, func); // Left side recursion
+        helpOrder('post', node.right, array, func); // Right side recursion
+        if (func !== undefined) {func(node)} // Apply function to node
+        array.push(node); // Add node to array
+        return array;
+    }
+}
 
 // Tree class
 class Tree {
@@ -116,11 +139,6 @@ class Tree {
         }
     }
     levelOrder(func){
-        // accepts another function as a parameter
-        // traverse the tree in breadth-first(?) level order
-        // passing each node as argument to the provided function) 
-        // return an array of values if no function is given. 
-        
         let nodeQueue = [];
         nodeQueue.push(this.root);
         let nodeCache = [];
@@ -131,11 +149,40 @@ class Tree {
             if (currentNode.left !== null) {nodeQueue.push(currentNode.left)}
             if (currentNode.right !== null) {nodeQueue.push(currentNode.right)} 
         }
-        return nodeCache;
+        let returnVal = func !== undefined ? 'success' : nodeCache;
+        return returnVal;
     }
-    
-        
-    
+    inOrder(func) {
+        let nodes = helpOrder('in', this.root, [], func);
+        if (func === undefined) { return nodes;}
+        return nodes;
+    }
+    preOrder(func) { 
+        let nodes = helpOrder('pre', this.root, [], func);
+        if (func === undefined){return nodes;}
+        return;
+    }
+    postOrder(func) { 
+        let nodes = helpOrder('post', this.root, [], func);
+        if (func === undefined){return nodes;}
+        return;
+    }
+    height(node) {
+        if (node === null) { return -1 } // Base case
+        if (node === undefined) { node = this.root } // Default to full height if called w/o arg
+        return 1+ Math.max(this.height(node.left), this.height(node.right)); // +1 to include current node
+    }
+    depthHelper(node, current, count) {
+        if (current === null) { return } // If node not found
+        if (node === current) { return count } // If node is found
+        // Else increment counter, search tree
+        if (node.value > current.value) { return this.depthHelper(node, current.right, count + 1) }
+        return this.depthHelper(node, current.left, count + 1)
+    }
+    depth(node) {
+        if (node === this.root){ return 0 } // Depth of root is 0
+        return this.depthHelper(node, this.root, 0);
+    }
 }
 
 let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 101, 102];
